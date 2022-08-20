d6b9e4beb39adfe444d3731b8f20c08c8e4caf17
Diego Carlos <diego_carlos110@hotmail.com>
2022-08-20 00:18:42 -0300

    models e admin criados

--- pizza/admin.py
+++ pizza/admin.py
@@ -4,6 +4,34 @@ from .models import Pedido, Pizza, Comentario, Ingredientes
 
 # Register your models here.
 
-@admin.register(Pizza)
+admin.site.register(Pedido)
+admin.site.register(Pizza)
+admin.site.register(Comentario)
+admin.site.register(Ingredientes)
+
+"""
 class PizzaAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'ingredientes', 'descricao_pizza')
+    list_display = ('nome', 'get_ingrediantes', 'descricao_pizza')
+    
+    def get_ingrediantes(self, request):
+        ing = request.objects.get('ingredientes')
+        return f'{self.ing[:50]}'
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'usuario', 'pizza', 'get_texto')
+
+    def get_texto(self, request):
+        obs = request.objects.get('observacao')
+        return f'{self.obs[:50]}'
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'pizza', 'usuario')
+
+
+
+@admin.register(Ingredientes)
+class IngredientesAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+"""
--- pizza/models.py
+++ pizza/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class Ingredientes(models.Model):
     nome = models.CharField(max_length=255)
-    pizza = models.ForeignKey("Pizza", on_delete=models.CASCADE)
+    descricao = models.TextField(blank=True)
 
     def __str__(self):
         return self.nome
@@ -16,26 +16,14 @@ class Ingredientes(models.Model):
     class Meta:
         verbose_name = 'ingrediente'
         verbose_name_plural = 'ingredientes'
+'''
+    def set_default_ingredientes():
+	    return Ingredientes.objects.get_or_create(nome='padrão')[0] #objeto ou boolena
+'''
 class Pizza(models.Model):
     nome = models.CharField(max_length=255)
-    ingredientes = models.ManyToManyField(Ingredientes)
-    descricao_pizza = models.TextField()
-
-class Comentario(models.Model):
-    titulo = models.CharField(max_length=255)
-    texto = models.TextField()
-    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-
-    def __str__(self):
-        return self.titulo
-
-    class Meta:
-        verbose_name = 'comentario'
-        verbose_name_plural = 'comentarios' 
-
-
-    
+    ingredientes = models.ManyToManyField(Ingredientes, blank=True,)
+    descricao_pizza = models.TextField(blank=True)
 
     def __str__(self):
         return self.nome
@@ -44,17 +32,37 @@ class Comentario(models.Model):
         verbose_name = 'pizza'
         verbose_name_plural = 'pizzas'
 
+    def get_ingredientes(self):
+        return "\n".join([p.ingredientes for p in self.ingrediente.all()])
+
+class Comentario(models.Model):
+    titulo = models.CharField(max_length=255)
+    texto = models.TextField()
+    pizza = models.ForeignKey(Pizza, null=True, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name = 'comentario'
+        verbose_name_plural = 'comentarios'     
+
+    
+
 
 class Pedido(models.Model):
     titulo = models.CharField(max_length=255)
-    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    pizza = models.ForeignKey(Pizza, on_delete=models.DO_NOTHING)
-    descricao = models.TextField()
+    usuario = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+    pizza = models.ForeignKey(Pizza, null=True, on_delete=models.DO_NOTHING)
+    observacao = models.TextField(blank=True)
 
     def __str__(self):
         return self.titulo
     class Meta:
         verbose_name = 'pedido'
         verbose_name_plural = 'pedidos'
+    
+    
 
 
